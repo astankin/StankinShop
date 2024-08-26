4164f1ceb43db93a2968acc081c6d460dfe43802
Atanas Stankin <a_stankin@abv.bg>
2024-08-26 22:33:07 +0300

    Added restriction to get all users and user info

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,7 +1,10 @@
 from django.http import JsonResponse
 from django.shortcuts import render
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
+
+from django.contrib.auth.models import User
 
 from .models import Product
 from .products import products
@@ -46,9 +49,17 @@ def getRoutes(request):
     return Response(routes)
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getUserProfile(request):
     user = request.user
     serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUsers(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
     return Response(serializer.data)
 
 @api_view(['GET'])
