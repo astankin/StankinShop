4f373bf1ad2d64faf1bce856c02e39d90ccc62b1
Atanas Stankin <a_stankin@abv.bg>
2024-09-01 18:24:00 +0300

    Added payment method screen

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -3,6 +3,8 @@ import {
     CART_ADD_ITEM, 
     CART_REMOVE_ITEM,
     CART_SAVE_SHIPPING_ADDRESS,
+
+    CART_SAVE_PAYMENT_METHOD,
  } from "../constants/cartConstants"; 
 
 
@@ -41,11 +43,11 @@ export const saveShippingAddress = (data) => (dispatch) => {
     localStorage.setItem('shippingAddress', JSON.stringify(data))
 };
 
-// export const savePaymentMethod = (data) => (dispatch) => {
-//     dispatch({
-//         type: CART_SAVE_PAYMENT_METHOD,
-//         payload: data,
-//     })
+export const savePaymentMethod = (data) => (dispatch) => {
+    dispatch({
+        type: CART_SAVE_PAYMENT_METHOD,
+        payload: data,
+    })
 
-//     localStorage.setItem('paymentMethod', JSON.stringify(data))
-// }
+    localStorage.setItem('paymentMethod', JSON.stringify(data))
+};
