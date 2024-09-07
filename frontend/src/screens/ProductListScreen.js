174d4c2c028d23c72d54a73776ef8f2553e6b01b
Atanas Stankin <a_stankin@abv.bg>
2024-09-07 15:55:06 +0300

    Added link to the product from the product list page

--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -1,125 +1,157 @@
-import React, { useEffect } from 'react';
-import { LinkContainer } from 'react-router-bootstrap';
-import { Table, Button, Row, Col } from 'react-bootstrap';
-import { useDispatch, useSelector } from 'react-redux';
-import Loader from '../components/Loader';
-import Message from '../components/Message';
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { LinkContainer } from "react-router-bootstrap";
+import { Table, Button, Row, Col } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 // import Paginate from '../components/Paginate';
-import { listProducts, deleteProduct, createProduct } from '../actions/productActions';
-import { PRODUCT_CREATE_RESET } from '../constants/productConstants';
-import { useNavigate, useLocation } from 'react-router-dom';
+import {
+  listProducts,
+  deleteProduct,
+  createProduct,
+} from "../actions/productActions";
+import { PRODUCT_CREATE_RESET } from "../constants/productConstants";
+import { useNavigate, useLocation } from "react-router-dom";
 
 function ProductListScreen() {
-    const dispatch = useDispatch();
-    const navigate = useNavigate();  // Use useNavigate to replace history.push
-    const location = useLocation();  // Use useLocation to replace history.location
+  const dispatch = useDispatch();
+  const navigate = useNavigate(); // Use useNavigate to replace history.push
+  const location = useLocation(); // Use useLocation to replace history.location
 
-    const productList = useSelector((state) => state.productList);
-    const { loading, error, products, page, pages } = productList;    
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products, page, pages } = productList;
 
-    const productDelete = useSelector(state => state.productDelete);
-    const { loading: loadingDelete, error: errorDelete, success: successDelete } = productDelete;
+  const productDelete = useSelector((state) => state.productDelete);
+  const {
+    loading: loadingDelete,
+    error: errorDelete,
+    success: successDelete,
+  } = productDelete;
 
-    const productCreate = useSelector(state => state.productCreate);
-    const { loading: loadingCreate, error: errorCreate, success: successCreate, product: createdProduct } = productCreate;
+  const productCreate = useSelector((state) => state.productCreate);
+  const {
+    loading: loadingCreate,
+    error: errorCreate,
+    success: successCreate,
+    product: createdProduct,
+  } = productCreate;
 
-    const userLogin = useSelector(state => state.userLogin);
-    const { userInfo } = userLogin;
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
 
-    let keyword = location.search;  // Replacing history.location.search with useLocation().search
+  let keyword = location.search; // Replacing history.location.search with useLocation().search
 
-    useEffect(() => {
-        dispatch({ type: PRODUCT_CREATE_RESET });
+  useEffect(() => {
+    dispatch({ type: PRODUCT_CREATE_RESET });
 
-        if (!userInfo.isAdmin) {
-            navigate('/login');
-        }
-    
-        if (successCreate) {
-            navigate(`/admin/product/${createdProduct._id}/edit`);
-        } else {
-            dispatch(listProducts(keyword));
-        }
+    if (!userInfo.isAdmin) {
+      navigate("/login");
+    }
 
-    }, [dispatch, navigate, userInfo, successDelete, successCreate, createdProduct, keyword]);
+    if (successCreate) {
+      navigate(`/admin/product/${createdProduct._id}/edit`);
+    } else {
+      dispatch(listProducts(keyword));
+    }
+  }, [
+    dispatch,
+    navigate,
+    userInfo,
+    successDelete,
+    successCreate,
+    createdProduct,
+    keyword,
+  ]);
 
-    const deleteHandler = (id) => {
-        if (window.confirm('Are you sure you want to delete this product?')) {
-            dispatch(deleteProduct(id));
-        }
-    };
+  const deleteHandler = (id) => {
+    if (window.confirm("Are you sure you want to delete this product?")) {
+      dispatch(deleteProduct(id));
+    }
+  };
 
-    const createProductHandler = () => {
-        dispatch(createProduct());
-    };
+  const createProductHandler = () => {
+    dispatch(createProduct());
+  };
 
-    return (
+  return (
+    <div>
+      <Row>
+        <Col>
+          <h1>Products</h1>
+        </Col>
+
+        <Col className="d-flex justify-content-end">
+          <Button className="my-3 mr-0" onClick={createProductHandler}>
+            <i className="fas fa-plus"></i> Create Product
+          </Button>
+        </Col>
+      </Row>
+
+      {loadingDelete && <Loader />}
+      {errorDelete && <Message variant="danger">{errorDelete}</Message>}
+
+      {loadingCreate && <Loader />}
+      {errorCreate && <Message variant="danger">{errorCreate}</Message>}
+
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
         <div>
-            <Row>
-                <Col>
-                    <h1>Products</h1>
-                </Col>
+          <Table striped bordered hover responsive className="table-sm">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>NAME</th>
+                <th>PRICE</th>
+                <th>CATEGORY</th>
+                <th>BRAND</th>
+                <th></th>
+              </tr>
+            </thead>
 
-                <Col className='d-flex justify-content-end'>
-                    <Button className='my-3 mr-0' onClick={createProductHandler}>
-                        <i className='fas fa-plus'></i> Create Product
+            <tbody>
+              {products.map((product) => (
+                <tr key={product._id}>
+                  <td>{product._id}</td>
+                  <td>
+                    <Link
+                      to={`/product/${product._id}`}
+                      className="text-decoration-none"
+                    >
+                      {product.name}
+                    </Link>
+                  </td>
+                  <td>${product.price}</td>
+                  <td>{product.category}</td>
+                  <td>{product.brand}</td>
+
+                  <td>
+                    <LinkContainer to={`/admin/product/${product._id}/edit`}>
+                      <Button variant="light" className="btn-sm">
+                        <i className="fas fa-edit"></i>
+                      </Button>
+                    </LinkContainer>
+
+                    <Button
+                      variant="danger"
+                      className="btn-sm"
+                      onClick={() => deleteHandler(product._id)}
+                    >
+                      <i className="fas fa-trash"></i>
                     </Button>
-                </Col>
-            </Row>
-
-            {loadingDelete && <Loader />}
-            {errorDelete && <Message variant='danger'>{errorDelete}</Message>}
-
-            {loadingCreate && <Loader />}
-            {errorCreate && <Message variant='danger'>{errorCreate}</Message>}
-
-            {loading ? (
-                <Loader />
-            ) : error ? (
-                <Message variant='danger'>{error}</Message>
-            ) : (
-                <div>
-                    <Table striped bordered hover responsive className='table-sm'>
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>NAME</th>
-                                <th>PRICE</th>
-                                <th>CATEGORY</th>
-                                <th>BRAND</th>
-                                <th></th>
-                            </tr>
-                        </thead>
-
-                        <tbody>
-                            {products.map(product => (
-                                <tr key={product._id}>
-                                    <td>{product._id}</td>
-                                    <td>{product.name}</td>
-                                    <td>${product.price}</td>
-                                    <td>{product.category}</td>
-                                    <td>{product.brand}</td>
-
-                                    <td>
-                                        <LinkContainer to={`/admin/product/${product._id}/edit`}>
-                                            <Button variant='light' className='btn-sm'>
-                                                <i className='fas fa-edit'></i>
-                                            </Button>
-                                        </LinkContainer>
-
-                                        <Button variant='danger' className='btn-sm' onClick={() => deleteHandler(product._id)}>
-                                            <i className='fas fa-trash'></i>
-                                        </Button>
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </Table>
-                    {/* <Paginate pages={pages} page={page} isAdmin={true} /> */}
-                </div>
-            )}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+          {/* <Paginate pages={pages} page={page} isAdmin={true} /> */}
         </div>
-    );
+      )}
+    </div>
+  );
 }
 
 export default ProductListScreen;
